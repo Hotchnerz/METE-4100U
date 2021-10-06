e2df0e57c0daeabe2a9c3ccfdd87c2abd0e5847b
totomyl <tommy.tran@uoit.net>
2021-10-06 11:37:23 -0400

    Update SARbot.ino

--- SARbot/SARbot.ino
+++ SARbot/SARbot.ino
@@ -6,10 +6,10 @@
 
 // declare variables
 // all digital pins on the due are interrupt pins
-#define left_encoder_channel_A 7 // for best performance attach all the encoder channels to interrupt pins 
-#define left_encoder_channel_B 8
-#define right_encoder_channel_A 9
-#define right_encoder_channel_B 10
+#define left_encoder_channel_A 9 // for best performance attach all the encoder channels to interrupt pins 
+#define left_encoder_channel_B 10
+#define right_encoder_channel_A 11
+#define right_encoder_channel_B 12
 Encoder left_wheel_encoder(left_encoder_channel_A, left_encoder_channel_B);
 Encoder right_wheel_encoder(right_encoder_channel_A, right_encoder_channel_B);
 float oldPosition  = -999;
@@ -37,7 +37,6 @@ int right_diagonal_ir_state = 1;
 // function to count the wheel rotations
 float countRotations(float encoder_position) {
   float rotations = encoder_position / 5800;
-  Serial.println(encoder_position);
   return rotations;
 }
 
@@ -58,24 +57,99 @@ void right_diagonal_state_isr() {
   right_diagonal_ir_state = digitalRead(right_diagonal_ir);
 }
 
-// correct speed function 
-void correct_speed(){
+// correct speed function
+void correctSpeed() {
   if (front_ir_state == 0) {
     left_motor_speed = 0;
-    right_motor_speed = 0; 
+    right_motor_speed = 0;
   } else if (left_diagonal_ir_state == 0) {
     left_motor_speed += 0.1;
   } else if (right_diagonal_ir_state == 0) {
     right_motor_speed += 0.1;
-  } else{
-    left_motor_speed = -0;
-    right_motor_speed = -0;
+  } else {
+    left_motor_speed = -50;
+    right_motor_speed = -50;
   }
 }
 
-// follow wall 
-void follow_wall(){
-  
+// left turn 90 deg
+void turnLeft() {
+  float initial_left_enc = left_wheel_encoder.read();
+  float initial_right_enc = right_wheel_encoder.read();
+  float initial_left_rot = countRotations(initial_left_enc);
+  float initial_right_rot = countRotations(initial_right_enc);
+  float left_rot = 0;
+  float right_rot = 0;
+  while ((left_rot <= 0.33) && (right_rot >= -0.33)) {
+    float left_enc = left_wheel_encoder.read();
+    float right_enc = right_wheel_encoder.read();
+    left_rot = initial_left_rot - countRotations(left_enc);
+    right_rot = initial_right_rot - countRotations(right_enc);
+    left_motor.setSpeed(-50);
+    right_motor.setSpeed(50);
+  }
+  left_motor.setSpeed(0);
+  right_motor.setSpeed(0);
+}
+
+// turn right 90 deg
+void turnRight() {
+  float initial_left_enc = left_wheel_encoder.read();
+  float initial_right_enc = right_wheel_encoder.read();
+  float initial_left_rot = countRotations(initial_left_enc);
+  float initial_right_rot = countRotations(initial_right_enc);
+  float left_rot = 0;
+  float right_rot = 0;
+  while ((left_rot >= -0.33) && (right_rot <= 0.33)) {
+    float left_enc = left_wheel_encoder.read();
+    float right_enc = right_wheel_encoder.read();
+    left_rot = initial_left_rot - countRotations(left_enc);
+    right_rot = initial_right_rot - countRotations(right_enc);
+    left_motor.setSpeed(50);
+    right_motor.setSpeed(-50);
+  }
+  left_motor.setSpeed(0);
+  right_motor.setSpeed(0);
+}
+
+// forward 1 cell step
+void fwdStep() {
+  float initial_left_enc = left_wheel_encoder.read();
+  float initial_right_enc = right_wheel_encoder.read();
+  float initial_left_rot = countRotations(initial_left_enc);
+  float initial_right_rot = countRotations(initial_right_enc);
+  float left_rot = 0;
+  float right_rot = 0;
+  while ((left_rot <= 0.93) && (right_rot <= 0.93)) {
+    if (front_ir_state == 0){
+      break; 
+    }
+    correctSpeed();
+    float left_enc = left_wheel_encoder.read();
+    float right_enc = right_wheel_encoder.read();
+    left_rot = countRotations(left_enc) - initial_left_rot;
+    right_rot = countRotations(right_enc) - initial_right_rot;
+    left_motor.setSpeed(left_motor_speed);
+    right_motor.setSpeed(right_motor_speed);
+  }
+  left_motor.setSpeed(0);
+  right_motor.setSpeed(0);
+}
+
+
+// follow wall
+void wallFollow() {
+  if ((front_ir_state == 1) && (right_ir_state == 0)) {
+    fwdStep(); 
+  } else if ((front_ir_state == 0) && (right_ir_state == 0)) {
+    turnLeft();
+  } else if (right_ir_state == 1) {
+    turnRight();
+    fwdStep(); 
+  } else {
+    left_motor.setSpeed(0);
+    right_motor.setSpeed(0);
+  }
 }
 
 void setup() {
@@ -91,10 +165,17 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(right_ir), right_state_isr, CHANGE);
   attachInterrupt(digitalPinToInterrupt(left_diagonal_ir), left_diagonal_state_isr, CHANGE);
   attachInterrupt(digitalPinToInterrupt(right_diagonal_ir), right_diagonal_state_isr, CHANGE);
+  delay(1000);
 }
 
 void loop() {
-  /*Serial.print("front ir: ");
+  float left_enc = left_wheel_encoder.read();
+  float right_enc = right_wheel_encoder.read();
+  float left_rot = countRotations(left_enc);
+  float right_rot = countRotations(right_enc);
+  wallFollow();
+  /*
+    Serial.print("front ir: ");
     Serial.print(front_ir_state);
     Serial.print(" left ir: ");
     Serial.print(left_ir_state);
@@ -104,22 +185,4 @@ void loop() {
     Serial.print(left_diagonal_ir_state);
     Serial.print(" right-d ir: ");
     Serial.println(right_diagonal_ir_state);*/
-
-  correct_speed(); 
-  left_motor.setSpeed(left_motor_speed);   
-  right_motor.setSpeed(right_motor_speed);  
-  /*left_motor.setSpeed(-50);
-    right_motor.setSpeed(-50);
-
-    if(front_ir_status == 0){
-    left_motor.setSpeed(0);   // Motor 1 runs forward at 50% speed. -128
-    right_motor.setSpeed(0);  // Motor 2 runs backward at 50% speed.
-    }*/
-  /*float newPosition = left_wheel_encoder.read();
-    if (newPosition != oldPosition) {
-    oldPosition = newPosition;
-    float rotation
-    }*/
-
-//TEST TEST TEST
 }
